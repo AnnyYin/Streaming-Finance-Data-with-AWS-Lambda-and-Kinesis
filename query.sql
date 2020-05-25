09645063dba9b9a116057b1c0e71b725d502f3db
AnnyYin <57237683+AnnyYin@users.noreply.github.com>
2020-05-25 03:05:42 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,6 @@
-SELECT
+SELECT name AS Stock,
+         (hour(cast(ts AS timestamp))-4) AS Hour,
+         max(high) AS Highest_Price
+FROM financedata22
+GROUP BY  hour(cast(ts AS timestamp)), name
+ORDER BY  hour(cast(ts AS timestamp)), name
