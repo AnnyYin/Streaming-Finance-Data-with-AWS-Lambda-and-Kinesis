b98450566c723eb61cda91c07350d4cd209d4a04
AnnyYin <57237683+AnnyYin@users.noreply.github.com>
2020-05-25 18:31:44 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,20 @@
-SELECT name AS Stock,
-         (hour(cast(ts AS timestamp))-4) AS Hour,
-         max(high) AS Highest_Price
-FROM financedata22
-GROUP BY  hour(cast(ts AS timestamp)), name
-ORDER BY  hour(cast(ts AS timestamp)), name
+WITH 
+    t1 AS (
+      SELECT name, high, ts, substring(ts,12,2) AS hour 
+      FROM financedata22
+      ), 
+    t2 AS (
+      SELECT name, substring(ts,12,2) AS hour, MAX(high) AS max_high 
+      FROM financedata22
+      GROUP BY  name, substring(ts, 12, 2)
+      )
+
+SELECT
+    t1.name AS Stock, 
+    t1.hour AS Hour, 
+    t2.max_high AS Hourly_High, 
+    substring(t1.ts,1,19) AS Datetime
+FROM 
+    t1 INNER JOIN t2 ON 
+    t1.name = t2.name AND t1.hour = t2.hour AND t1.high = t2.max_high
+ORDER BY  Stock, Hour, Datetime
